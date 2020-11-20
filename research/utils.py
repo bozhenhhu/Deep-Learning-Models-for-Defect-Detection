6b2ce32e42b34aaa7179e20a0606cc10075ac09d
bozhenhhu <37445920+bozhenhhu@users.noreply.github.com>
2020-11-20 15:49:39 +0800

    Update utils.py

--- research/utils.py
+++ research/utils.py
@@ -85,3 +85,24 @@ def jpg2png():
         im.save(png)
         # print(jpg)
 
+ #rorate an img 90 degree
+class Solution:
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: void Do not return anything, modify matrix in-place instead.
+        """
+        m = len(matrix)
+        n = len(matrix[0])
+        for i in range(m):
+            for j in range(i + 1):
+                self.swap(matrix, i, j, j, i)
+        for i in range(n // 2):
+            for j in range(m):
+                self.swap(matrix, j, i, j, n - 1 - i)
+        return matrix
+
+    def swap(self, matrix, i1, j1, i2, j2):
+        temp = matrix[i1][j1]
+        matrix[i1][j1] = matrix[i2][j2]
+        matrix[i2][j2] = temp
