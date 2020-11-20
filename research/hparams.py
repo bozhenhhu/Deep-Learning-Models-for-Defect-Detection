25fdf73b7cc323ca5c9a7c3ce09953de16391bc8
bozhenhhu <37445920+bozhenhhu@users.noreply.github.com>
2020-11-20 13:37:14 +0800

    Update hparams.py

--- research/hparams.py
+++ research/hparams.py
@@ -10,6 +10,7 @@ def Hparams():
     #whole project
     parser.add_argument('--width', type=int, default=256, help='image width for input')
     parser.add_argument('--height', type=int, default=256, help='image height for input')
+    parser.add_argument('--Time', type=int, default=16)
     parser.add_argument('--seed', type=int, default=2020, help='seed for this program')
     #pca
     parser.add_argument('--pca_num', type=int, default=6, help='decide the num of images saved by PCA')
