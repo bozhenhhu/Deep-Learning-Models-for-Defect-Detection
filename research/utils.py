60a3b3bae2ef10218269781ced36bca6cbce7c2e
bozhenhhu <37445920+bozhenhhu@users.noreply.github.com>
2021-04-07 03:47:18 +0800

    Update utils.py

--- research/utils.py
+++ research/utils.py
@@ -3,6 +3,45 @@ import scipy.io as sio
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
+import logging
+import os
+import warnings
+import random
+import tensorflow as tf
+
+def seeds(seed):
+    warnings.filterwarnings("ignore")
+    random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    np.random.seed(seed)
+    tf.random.set_seed(seed)
+
+
+def set_logger(log_path):
+    """
+    Set the logger to log info in terminal and file `log_path`.
+    Example:
+    ```
+    logging.info("Starting training...")
+    ```
+    Args:
+        log_path: (string) where to log
+    """
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    if not logger.handlers:
+        # Logging to a file
+        file_handler = logging.FileHandler(log_path)
+        file_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s: %(message)s'))
+        file_handler.setLevel(logging.INFO)
+        logger.addHandler(file_handler)
+
+        # Logging to console
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(logging.Formatter('%(message)s'))
+        stream_handler.setLevel(logging.INFO)
+        logger.addHandler(stream_handler)
 
 
 def on_mouse(event, x,y, flags , params):
