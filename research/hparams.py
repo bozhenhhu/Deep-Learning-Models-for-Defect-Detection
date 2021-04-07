bc9ef8942130262e691402470d9715fd8c0c08a4
bozhenhhu <37445920+bozhenhhu@users.noreply.github.com>
2021-04-07 17:00:16 +0800

    Update hparams.py

--- research/hparams.py
+++ research/hparams.py
@@ -4,10 +4,12 @@ import argparse
 def Hparams():
     parser = argparse.ArgumentParser()
     #network
-    parser.add_argument('--batchsize', type=int, default=2)
+    parser.add_argument('--batchsize', type=int, default=16)
     parser.add_argument('--dropout_rate', type=float, default=0.5)
     parser.add_argument('--act', type=str, default='relu', help='network activation')
     parser.add_argument('--l_weight', type=float, default=2., help='bce and dice weight')
+    parser.add_argument('--three_downsampling', type=bool, default=False)
+    parser.add_argument('--use_pca', type=bool, default=True)
 
     #whole project
     parser.add_argument('--width', type=int, default=256, help='image width for input')
@@ -16,8 +18,8 @@ def Hparams():
 
     parser.add_argument('--seed', type=int, default=2020, help='seed for this program')
     parser.add_argument('--mode', type=str, default='train', help='train,test')
-    parser.add_argument('--data_mode', type=str, default='3D', help='2D,3D')
-    parser.add_argument('--ids', type=int, default=28)
+    parser.add_argument('--data_mode', type=str, default='2D', help='2D,3D')
+    parser.add_argument('--ids', type=int, default=0)
     parser.add_argument('--sub_frame', type=str, default='first', help='first, last, last_mean, None')
     parser.add_argument('--mean_len', type=int, default=-10)
     parser.add_argument('--shuffle', type=bool, default=True)
@@ -28,8 +30,7 @@ def Hparams():
     parser.add_argument('--heating_rate', type=int, default=4, help='4, or 5')
     parser.add_argument('--sample_rate', type=int, default=2)
 
-    parser.add_argument('--model_mode', type=str, default='UNetpa', help='UNet++,UNet,UNet+pca, UNetpa')
-    parser.add_argument('--batch_size', type=int, default=1)
+    parser.add_argument('--model_mode', type=str, default='UNet++', help='UNet++,UNet,UNet+pca, UNetpa')
     parser.add_argument('--epoch', type=int, default=30)
 
     parser.add_argument('--gpu_id', type=str, default='0')
